9d521d567aec61594e0d7fcb8cbaeb3aa120b4a9
digitrom <roma.bobrovski@gmail.com>
2023-05-28 22:55:38 +0200

    HW5 - in propgress

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -29,6 +29,10 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
 
+        // onChange && onChange(e)
+        // onChangeChecked && onChangeChecked(e.currentTarget.checked)
+        onChange?.(e)
+        onChangeChecked?.(e.currentTarget.checked)
     }
 
     const finalInputClassName = s.checkbox
