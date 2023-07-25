18b4d12658357383b9cb8ccae0e4713825301976
digitrom <roma.bobrovski@gmail.com>
2023-07-25 14:06:56 +0200

    HW9 - done-correct

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -30,20 +30,22 @@ setShow(true)
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
         setShow(false)
     }
+    let formatterDate = new Intl.DateTimeFormat("ru", );
+
     let formatterTime = new Intl.DateTimeFormat("ru", {
         hour: "numeric",
         minute: "numeric",
         second: "numeric"
     });
-
     const stringTime = formatterTime.format(date)|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date?.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
+    const stringDate = formatterDate.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     let formatterWeekDay = new Intl.DateTimeFormat("en-US", {
         weekday: "long",
         // month: "long"
     });
+
     let formatterMonth = new Intl.DateTimeFormat("en-US", {
         month: "long"
     });
